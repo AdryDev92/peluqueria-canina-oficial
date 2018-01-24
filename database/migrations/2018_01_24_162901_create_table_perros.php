3a29b313d363037415d26c10847dad3379d690cb
adry <tazzmaniac51@gmail.com>
2018-01-24 17:48:22 +0100

    first migration completed

---
+++ database/migrations/2018_01_24_162901_create_table_perros.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePerros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('create_table_perros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('race');
+            $table->decimal('weight');
+            $table->string('cut');
+            $table->text('notes');
+            $table->string('color');
+            $table->string('image');
+            $table->rememberToken();
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('create_table_perros');
+    }
+}
